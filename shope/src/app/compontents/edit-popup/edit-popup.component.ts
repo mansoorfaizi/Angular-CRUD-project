063220afebe22be7b367634e37428cd95e539ad5
Mansoor Faizi <mansoorfaizi.cs05@gmail.com>
2024-02-21 22:25:16 +0430

    project setup completed

---
+++ shope/src/app/compontents/edit-popup/edit-popup.component.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
+import { Product } from '../../../types';
+import { FormsModule } from '@angular/forms';
+import { RatingModule } from 'primeng/rating';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-edit-popup',
+  standalone: true,
+  imports: [
+    DialogModule,
+    CommonModule,
+    FormsModule,
+    RatingModule,
+    ButtonModule,
+  ],
+  templateUrl: './edit-popup.component.html',
+  styleUrl: './edit-popup.component.css',
+})
+export class EditPopupComponent {
+  @Input() display: boolean = false;
+  @Input() header!: string;
+  @Output() displayChange = new EventEmitter<boolean>();
+  @Output() confirm = new EventEmitter<Product>();
+
+  @Input() product: Product = {
+    name: '',
+    image: '',
+    price: '',
+    rating: 0,
+  };
+
+  onConfirm() {
+    this.confirm.emit(this.product);
+    this.display = false;
+    this.displayChange.emit(this.display)
+  }
+
+  onCancel() {
+    this.display = false;
+    this.displayChange.emit(this.display)
+  }
+}
---
+++ shope/src/app/services/products.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
+import { PaginationParams, Products } from '../../types';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductsService {
+  constructor(private apiService: ApiService) {}
+  getProducts = (url: string, params: PaginationParams): Observable<Products> => {
+    return this.apiService.get(url, {
+      params,
+      responseType: 'json',
+    });
+  };
+
+  addProducts = (url: string, body: any): Observable<any> => {
+    return this.apiService.post(url, body, {});
+  };
+
+  editProducts = (url: string, body: any): Observable<any> => {
+    return this.apiService.put(url, body, {});
+  };
+
+  deleteProducts = (url: string): Observable<any> => {
+    return this.apiService.delete(url, {});
+  };
+}
